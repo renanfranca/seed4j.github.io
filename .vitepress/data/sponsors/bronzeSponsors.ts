1bfe482195763e57757e7049652aa83d1a177245
renanfranca <6011616+renanfranca@users.noreply.github.com>
2025-09-12 13:35:56 +0000

    Prefetch-sponsors: update sponsor data

--- .vitepress/data/sponsors/bronzeSponsors.ts
+++ .vitepress/data/sponsors/bronzeSponsors.ts
@@ -1,3 +1,10 @@
 import type { Sponsor } from './sponsors';
 
-export const bronze: Sponsor[] = [];
+export const bronze: Sponsor[] = [
+  {
+    memberId: 720740,
+    name: 'Geoffray Gruel',
+    url: 'https://opencollective.com/guest-b627ebd3',
+    img: '/sponsors/guest-b627ebd3-720740.png',
+  },
+];
